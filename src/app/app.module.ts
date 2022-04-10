86f2d8e50ab84fe43c80e9d69a9d6d522ca4ff89
Develper-Gitonga <aizensalim52@gmail.com>
2022-04-10 22:28:15 +0300

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { GenFormComponent } from './gen-form/gen-form.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteLikesComponent } from './quote-likes/quote-likes.component';
+import { HighlightDirective } from './highlight.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     GenFormComponent,
     QuoteComponent,
-    QuoteLikesComponent
+    QuoteLikesComponent,
+    HighlightDirective,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
